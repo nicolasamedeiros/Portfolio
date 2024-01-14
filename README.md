0ab7ec0d3987505eced34db182acad5e873853be
Nicolas Almeida <nicolas.amedeiros@gmail.com>
2024-01-14 11:47:26 -0300

    Portfolio

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Portfolio
+
+<h2>This is a personal portfolio. Developed using html, css and js. You can see clicking in the link below</h2>
+
+LINK => https://nicolasamedeiros.github.io/Crypto-Price/
---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*typing animation */
+
+var typed = new Typed(".typing",{
+    strings:["","Engenheiro de Software", "Administrador de Banco de Dados", "Desenvolvedor Web", "Cientista de Dados"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
